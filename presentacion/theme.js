6864975228098bb985872645022daa4cb480311e
David Rearte <davidrearte@Davids-MacBook-Pro.local>
2019-04-26 15:51:15 -0300

    agregado deck

---
+++ presentacion/theme.js
@@ -0,0 +1,18 @@
+import { condensed as theme } from 'mdx-deck/themes'
+
+export default {
+  ...theme,
+  colors: {
+    background: '#4e385d',
+    text: '#eeeeee'
+  }
+  // link: 'pink'
+  // font: 'Roboto, sans-serif'
+
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+
+}
